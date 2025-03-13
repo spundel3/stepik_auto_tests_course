c2b009c8fd3c9c44dca83cbd933673bba3170190
spundel3 <spundel3@gmail.com>
2025-03-13 16:20:12 +0300

    добавил задачу 2.4_step8.py

---
+++ 2.4_step8.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/explicit_wait2.html")
+    price = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "$100")
+    )
+    browser.find_element(By.CSS_SELECTOR, "#book").click()
+
+    x = browser.find_element(By.CSS_SELECTOR, "#input_value").text
+    y = calc(x)
+    
+    browser.find_element(By.CSS_SELECTOR, "#answer").send_keys(y)
+
+    browser.find_element(By.CSS_SELECTOR, "#solve").click()
+
+    time.sleep(5)
+finally:
+    browser.quit()
